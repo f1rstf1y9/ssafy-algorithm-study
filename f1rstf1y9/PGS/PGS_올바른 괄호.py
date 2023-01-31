4bbe5207582d07f1df6ae9ded6040c03c7c440e6
f1rstf1y9 <f1rstf1y9@naver.com>
2023-01-31 10:14:19 +0900

    [PGS] 올바른 괄호 / LV2
    https://school.programmers.co.kr/learn/courses/30/lessons/12909

---
+++ f1rstf1y9/PGS/PGS_올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        else:
+            if not stack:
+                return False
+            if stack.pop() != "(":
+                return False
+    return False if stack else True
