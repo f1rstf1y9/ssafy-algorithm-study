d3f5c8b14b470fe256c3f27363ee4b08299b56c7
f1rstf1y9 <f1rstf1y9@naver.com>
2023-02-02 17:10:17 +0900

    [BOJ] 최대 힙 / 실버2
    https://www.acmicpc.net/problem/11279

---
+++ f1rstf1y9/BOJ/BOJ_11279_최대 힙.py
@@ -0,0 +1,17 @@
+from heapq import heappop, heappush
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(-1*heappop(heap))
+        else:
+            print(0)
+    else:
+        heappush(heap, -x)
